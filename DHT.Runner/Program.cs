f7dbdd352d1f8ab604e18bc21c79c0888e7551f1
Olivia <olivia_git@outlook.com>
2017-04-15 18:00:53 +0200

    Adding NodeServer runner.

---
+++ DHT.Runner/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DHT.Runner
+{
+    using System;
+    using Grpc.Core;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = int.Parse(args[0]);
+
+            var server = new Server
+            {
+                Services = { Dhtproto.DhtProtoService.BindService(new NodeServer()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("NodeServer server listening on port " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadLine();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
