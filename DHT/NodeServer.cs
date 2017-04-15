567575a403a384e3de5597781a33633de738100c
Olivia <olivia_git@outlook.com>
2017-04-15 17:51:27 +0200

    Implementing NodeServer

---
+++ DHT/NodeServer.cs
@@ -0,0 +1,19 @@
+﻿namespace DHT
+{
+    using System.Threading.Tasks;
+    using Dhtproto;
+    using grpc = global::Grpc.Core;
+
+    public class NodeServer : DhtProtoService.DhtProtoServiceBase
+    {
+        public override Task<StringMessage> SayHello(StringMessage request, grpc::ServerCallContext context)
+        {
+            var stringMessage = new StringMessage()
+            {
+                Message = "Hello"
+            };
+
+            return Task.FromResult<StringMessage>(stringMessage);
+        }
+    }
+}
