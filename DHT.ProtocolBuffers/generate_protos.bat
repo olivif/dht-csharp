66cfd1bd7f0aa1cc303b6eaa9120455302acebca
Olivia <olivia_git@outlook.com>
2017-04-15 17:37:34 +0200

    Moving proto file.

--- DHT.ProtocolBuffers/generate_protos.bat
+++ DHT.ProtocolBuffers/generate_protos.bat
@@ -36,6 +36,6 @@ cd /d %~dp0
 
 set TOOLS_PATH=..\packages\Grpc.Tools.1.2.2\tools\windows_x86
 
-%TOOLS_PATH%\protoc.exe -I ../protos --csharp_out DhtProto  ../protos/dht.proto --grpc_out DhtProto --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
+%TOOLS_PATH%\protoc.exe -I protos --csharp_out DhtProto  protos/dht.proto --grpc_out DhtProto --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
 
 endlocal
