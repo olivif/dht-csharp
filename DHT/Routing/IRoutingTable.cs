61c220034964263902602ad4272e83684776d31f
Olivia <olivia_git@outlook.com>
2017-04-16 12:43:37 +0200

    Addign routing table.

---
+++ DHT/Routing/IRoutingTable.cs
@@ -0,0 +1,16 @@
+﻿namespace DHT.Routing
+{
+    using System.Collections.Generic;
+    using DHT.Nodes;
+
+    /// <summary>
+    /// Routing interface
+    /// </summary>
+    public interface IRoutingTable
+    {
+        /// <summary>
+        /// List of node info in the network
+        /// </summary>
+        IList<NodeInfo> Nodes { get; set; }
+    }
+}
---
+++ DHT/Routing/RoutingTable.cs
@@ -0,0 +1,13 @@
+﻿namespace DHT.Routing
+{
+    using System.Collections.Generic;
+    using DHT.Nodes;
+
+    /// <summary>
+    /// Routing table implementation
+    /// </summary>
+    public class RoutingTable : IRoutingTable
+    {
+        public IList<NodeInfo> Nodes { get; set; }
+    }
+}
