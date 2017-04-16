02b6e8d9e86beeb5332f176f0887d606a36f6c81
Olivia <olivia_git@outlook.com>
2017-04-16 23:02:06 +0200

    Adding factory tests.

---
+++ DHT.Tests/NodesProtocol/NodeServerClientFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿namespace DHT.Tests.NodesProtocol
+{
+    using DHT.Nodes;
+    using DHT.NodesProtocol;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NodeServerClientFactoryTests
+    {
+        [TestMethod]
+        public void NodeServerClientFactory_CreateLocalClient()
+        {
+            // Arrange
+            var factory = new NodeServerClientFactory();
+
+            // Act
+            var client = factory.CreateLocalClient();
+
+            // Assert
+            Assert.IsInstanceOfType(client, typeof(LocalNodeServerClient));
+        }
+
+        [TestMethod]
+        public void NodeServerClientFactory_CreateRemoteClient()
+        {
+            // Arrange
+            var factory = new NodeServerClientFactory();
+            var nodeInfo = new NodeInfo();
+
+            // Act
+            var client = factory.CreateRemoteClient(nodeInfo);
+
+            // Assert
+            Assert.IsNotInstanceOfType(client, typeof(LocalNodeServerClient));
+        }
+    }
+}
