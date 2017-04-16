72886d1006efeceae76b9b99f7aaa2423a15c1e7
Olivia <olivia_git@outlook.com>
2017-04-16 20:17:04 +0200

    Moving routing table to common. Adding NodeRunner.

--- DHT.Network.NodeRunner/Program.cs
+++ DHT.Network.NodeRunner/Program.cs
@@ -1,15 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DHT.Network.NodeRunner
+﻿namespace DHT.Network.NodeRunner
 {
-    class Program
+    using System;
+    using System.Linq;
+    using DHT.Nodes;
+    using DHT.Routing;
+    using Grpc.Core;
+
+    public class Program
     {
         static void Main(string[] args)
         {
+            // Create routing table
+            var routingTable = RoutingTableFactory.FromFile("routingTable.txt");
+
+            // Get node to run
+            var nodeId = UInt32.Parse(args[0]);
+            var nodeInfo = routingTable.Nodes.FirstOrDefault(n => n.NodeId == nodeId);
+
+            // Start that node 
+            var server = StartNodeServer(nodeInfo, routingTable);
+
+            // Wait for input before killing it
+            Console.ReadKey();
+
+            KillNodeServer(server);
+        }
+
+        private static Server StartNodeServer(NodeInfo node, IRoutingTable routingTable)
+        {
+            var nodeServer = new NodeServer(node, routingTable);
+
+            var server = new Server
+            {
+                Services = { Dhtproto.DhtProtoService.BindService(nodeServer) },
+                Ports = { new ServerPort(node.HostName, node.Port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("NodeServer server listening on {0}:{1} ", node.HostName, node.Port);
+            Console.WriteLine();
+
+            return server;
+        }
+
+        private static void KillNodeServer(Server nodeServer)
+        {
+            nodeServer.ShutdownAsync().Wait();
+
+            Console.WriteLine("NodeServer server killed");
+            Console.WriteLine();
         }
     }
 }
