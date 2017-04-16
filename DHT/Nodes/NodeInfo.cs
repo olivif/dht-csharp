4c7aaede1e86397bb3bdc98691804c6df7494271
Olivia <olivia_git@outlook.com>
2017-04-16 12:31:16 +0200

    Adding NodeInfo.

---
+++ DHT/Nodes/NodeInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace DHT.Nodes
+{
+    using System;
+
+    /// <summary>
+    /// Stores info about a node in the DHT network
+    /// </summary>
+    public class NodeInfo
+    {
+        /// <summary>
+        /// 32 bit node id
+        /// </summary>
+        /// <remarks>
+        /// Being explicit about the 32 bit declaration
+        /// since it's crucial to how routing will work between DHT nodes.
+        /// </remarks>
+        public Int32 NodeId { get; set; }
+
+        /// <summary>
+        /// Host name (or IP)
+        /// </summary>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Port running on
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
